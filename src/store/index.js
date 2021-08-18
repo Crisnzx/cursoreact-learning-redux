77c3cbde0306a0aa538cd2e6996e1162a55653ec
Cristovao Neto <cristovaon13@gmail.com>
2021-08-18 09:25:35 -0300

    working with a more complex state object

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,29 @@
 import { createStore } from 'redux';
 
-function counterReducer(state = { counter: 0 }, action) {
+// Redux doesn't merge your new state with the old state, instead it'll replace everything, so
+// it's important to overwrite with the old state the data that you won't change
+
+function counterReducer(state = { counter: 0, showCounter: true }, action) {
   switch (action.type) {
     case 'increment':
       return {
         counter: state.counter + 1,
+        showCounter: state.showCounter,
       };
     case 'decrement':
       return {
         counter: state.counter - 1,
+        showCounter: state.showCounter,
+      };
+    case 'increase':
+      return {
+        counter: state.counter + action.amount,
+        showCounter: state.showCounter,
+      };
+    case 'toggle':
+      return {
+        counter: state.counter,
+        showCounter: !state.showCounter,
       };
     default:
       return state;
